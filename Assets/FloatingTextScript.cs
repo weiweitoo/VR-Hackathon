a08c50f9d5be853beeb98a93694be0c1f8285567
weiweitoo <sengweitoo@gmail.com>
2018-12-14 15:27:45 +0800

    Add floaating text. WIP for generating flaoting text with manager

---
+++ Assets/FloatingTextScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingTextScript : MonoBehaviour {
+
+	public string text;
+
+	void Start () {
+		Transform x = transform.Find("Face 1").Find("Text");
+		Debug.Log(x);
+		Text textComponent = x.GetComponent<Text>();
+		textComponent.text = text;
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Assets/PostManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostManagerScript : MonoBehaviour {
+
+	public GameObject postPrefab;
+	public List<string> post = new List<string>();
+
+	void Start () {
+		FloatingTextScript floatingTextScript;
+		for (int y = 0; y < 1; y++) 
+		{
+            GameObject textObj = Instantiate(postPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+			floatingTextScript = textObj.GetComponent<FloatingTextScript>();
+			floatingTextScript.text = "12dede3";
+			Debug.Log(textObj);
+        }
+	}
+	
+	void Update () {
+		
+	}
+}
