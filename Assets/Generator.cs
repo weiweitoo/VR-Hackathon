e1195de9df67103a9b8a90fd91ee237de65fd3ce
weiweitoo <sengweitoo@gmail.com>
2018-12-16 04:42:59 +0800

    Add graph

---
+++ Assets/Generator.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+	public GameObject prefab;
+	public float maxSize = 3.0f;
+	public float minSize = 1.0f;
+	public float spreadRange = 80.0f;
+	public float spreadCheck = 100.0f;
+
+	// Use this for initialization
+	void Start () {
+		List<GameObject> oList = new List<GameObject>();
+		List<string> prbList = new List<string>();
+
+		prbList.Add("Overheat");				//1
+		prbList.Add("Inner Overheat");			//2
+		prbList.Add("Outer Overheat");			//3
+		prbList.Add("Cracks");					//4
+		prbList.Add("Transmission Faults");		//5
+		prbList.Add("Power Leak");				//6
+		prbList.Add("Segmental Faults");		//7
+		prbList.Add("Power Spike");				//8
+		prbList.Add("Fan Backstop");			//9
+		prbList.Add("Cooling Systen Faults");	//10
+		prbList.Add("Pipe Damaged");			//11
+		prbList.Add("Power Overload");			//12
+		prbList.Add("Turbine Blades Wore");		//13
+		prbList.Add("Wire Coat Damaged");		//14
+		prbList.Add("Wire Coat Wore");			//15
+
+		foreach(string pbs in prbList) {
+			//Randomly select a size - TEMP
+			float randomScale = Random.Range(minSize,maxSize);
+			bool placed = false;
+			//Randomly select a color - TEMP
+			Color clr = new Color(Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), Random.Range(0.0f,1.0f), 0.5f);
+
+			while(!placed) {
+				//Width spread
+				float xx = Random.Range(-spreadRange + randomScale * 4.0f, spreadRange - randomScale * 4.0f);
+				float zz = Random.Range(-spreadRange + randomScale * 4.0f, spreadRange - randomScale * 4.0f);
+
+				//Height spread
+				float yy = Random.Range(-spreadRange/2, spreadRange/2);
+
+				foreach(GameObject obj in oList) {
+					float distance = Vector3.Distance(new Vector3(xx,yy,zz), obj.transform.position);
+					if (distance < spreadRange + obj.transform.localScale.x * 2.0f) {
+						//Debug.Log("Too close!");
+						continue;
+					}
+				}
+
+				//Add Sphere to scenario
+				GameObject o = Object.Instantiate(prefab, new Vector3(xx,yy,zz), Quaternion.identity);
+				o.GetComponent<FloatingTextScript>().text = pbs + "\n" + System.Math.Round((randomScale - minSize)/(maxSize - minSize)*100) + "%";
+				o.GetComponent<RectTransform>().localScale = new Vector3(randomScale,randomScale,randomScale);
+        		o.GetComponent<Renderer>().material.SetColor("_Color", clr);
+				oList.Add(o);
+				placed = true;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SimplePlayerFly.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerFly : MonoBehaviour {
+
+    public float walkSpeed = 2;
+    public float runSpeed = 4;
+    public float gravity = 9.8f;
+    public float lookSpeed = 45;
+    public bool invertY = true;
+    public Transform cameraPivot;
+
+    float speed;
+    Vector3 movement, finalMovement;
+    CharacterController controller;
+    Quaternion targetPivotRotation;
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        targetPivotRotation = Quaternion.identity;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateTranslation();
+        UpdateLookRotation();
+    }
+
+    void UpdateLookRotation()
+    {
+        var x = Input.GetAxis("Mouse Y");
+        var y = Input.GetAxis("Mouse X");
+
+        x *= invertY ? -1 : 1;
+
+        targetPivotRotation = cameraPivot.transform.rotation * Quaternion.AngleAxis(x * lookSpeed * Time.deltaTime, Vector3.right) * Quaternion.AngleAxis(y * lookSpeed * Time.deltaTime, Vector3.up);
+        cameraPivot.transform.rotation = Quaternion.Slerp(cameraPivot.transform.rotation, targetPivotRotation, Time.deltaTime * 15);
+        float z = cameraPivot.transform.eulerAngles.z;
+        cameraPivot.transform.Rotate(0, 0, -z);
+    }
+
+
+    void UpdateTranslation()
+    {
+        if (true)
+        {
+            var x = Input.GetAxis("Horizontal");
+            var z = Input.GetAxis("Vertical");
+            var run = Input.GetKey(KeyCode.LeftShift);
+
+            var translation = new Vector3(x, 0, z);
+            speed = run ? runSpeed : walkSpeed;
+            movement = cameraPivot.transform.TransformDirection(translation * speed);
+        }
+        else
+        {
+            movement.y -= gravity * Time.deltaTime;
+        }
+        finalMovement = Vector3.Lerp(finalMovement, movement, Time.deltaTime * 25);
+        controller.Move(finalMovement * Time.deltaTime);
+    }
+}
